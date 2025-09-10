9da4c5a513f99650216b0c78d28f9b664f84777f
York Yu <n11453559@gmail.com>
2025-09-10 12:11:57 +1000

    add filter functionality to Categories component

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,19 @@ const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
   const [categories, setCategories] = useState(allCategories)
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu)
+      return
+    }
+    const newItems = menu.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Categories = ({ categories }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
       {categories.map((category) => (
-        <button type="button" className="btn" key={category}>
+        <button
+          type="button"
+          className="btn"
+          key={category}
+          onClick={() => filterItems(category)}
+        >
           {category}
         </button>
       ))}
