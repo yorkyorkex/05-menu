ee5697699621f02900cb223649401ca177a8d03b
York Yu <n11453559@gmail.com>
2025-09-10 12:08:45 +1000

    add Categories component to display menu categories

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import Title from './Title'
 import { use } from 'react'
 import menu from './data'
 import Menu from './Menu'
+import Categories from './Categories'
+
+/* const tempCategories = menu.map((item) => item.category)
+const tempSet = new Set(tempCategories)
+const tempItems = ['all', ...tempSet]
+
+console.log(tempCategories)
+console.log(tempSet)
+console.log(tempItems) */
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
