2e34be5a6c9a16d2bc29deb59577729bc9c87541
York Yu <n11453559@gmail.com>
2025-09-10 11:37:05 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from 'react'
+import Title from './Title'
+
+const App = () => {
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our Menu" />
+      </section>
+    </main>
+  )
+}
+export default App
