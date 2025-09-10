a1c31a14a075eaa7ff9422b3da3c71055d3ba5fa
York Yu <n11453559@gmail.com>
2025-09-10 11:50:14 +1000

    add Menu and MenuItem components to display menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import React from 'react'
 import Title from './Title'
+import { use } from 'react'
+import menu from './data'
+import Menu from './Menu'
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu)
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
